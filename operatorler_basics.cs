1f7fd305df58a3a69a70fe895714503c0a243b23
Alperen Atar <71658129+EmparrioR@users.noreply.github.com>
2023-02-21 14:03:09 -0800

    Add files via upload

---
+++ operatorler_basics.cs
@@ -0,0 +1,54 @@
+﻿namespace operatorler
+{
+    class operatorler_basics{
+        static void Main(string[] args){
+            
+            // Atama ve işlemli atama
+
+            int y = 5;
+            int x = 3;
+            y+=2;
+
+            Console.WriteLine(y);
+            y+=2;
+            Console.WriteLine(y);
+            y/=1;
+            Console.WriteLine(y);
+            x*=2;
+            Console.WriteLine(x);
+
+            // Mantıksal Operatörler
+
+            bool isSuccess = true;
+            bool isCompleted = false;
+
+            if (isSuccess && isCompleted){
+                Console.WriteLine("Perfect");
+            } if(isSuccess || isCompleted){
+                Console.WriteLine("Great");
+            } if(isSuccess && !isCompleted){
+                Console.WriteLine("Fine");
+            }
+
+            // Mantıksal Operatörler
+            // <,>,<=,>=,==,!=
+
+            int a = 3;
+            int b = 5;
+            bool sonuc = a<b;
+
+            Console.WriteLine(sonuc);
+            sonuc = a>b;
+            Console.WriteLine(sonuc);
+            sonuc = a<=b;
+            Console.WriteLine(sonuc);
+            sonuc = a==b;
+            Console.WriteLine(sonuc);
+
+            // Aritmetik Operatörler
+            // +,-,*,/,%,+=,-=,/=,*=
+
+
+        }
+    }
+}
