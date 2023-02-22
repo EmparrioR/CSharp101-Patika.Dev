28576479ed4ab45555ae6f560a57029ac6d6528a
Alperen Atar <71658129+EmparrioR@users.noreply.github.com>
2023-02-22 17:43:29 +0300

    Add files via upload

---
+++ try_catch.cs
@@ -0,0 +1,56 @@
+﻿namespace hataYonetimi
+{
+    class try_catch{
+        static void Main(string [] args){
+
+            
+            try
+            {   
+                // Kontrol etmek istedimğimiz kod bloğunu yazarız
+                Console.WriteLine("Bir sayı giriniz");
+                int a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Girdiğiniz sayı: " + a);
+ 
+            }
+            catch (Exception ex)
+            {   
+                // ex.Message hata mesajını yazdırır
+                Console.WriteLine("Hata: " + ex.Message.ToString());
+                
+            }
+            finally
+            {
+                // Hata alsın veya almasın final bloğu çalışır
+                Console.WriteLine("İşlem Tamamlandı !");
+            }
+
+            // Hata çıktısı olarak istediğimiz şeyi yazdırabilme
+            try{    
+
+               // int a = int.Parse(null);
+               // int a = int.Parse("test");
+               int a = int.Parse("-2000000000000000000");
+
+            }
+            catch(ArgumentException e){
+                
+                Console.WriteLine("Boş değer girdiniz");
+                Console.WriteLine(e);
+
+            }catch(FormatException e){
+                
+                Console.WriteLine("Veri tipi hatası");
+                Console.WriteLine(e);
+
+            }catch(OverflowException e){
+
+                Console.WriteLine("Çok küçük veya büyük bir değer girdiniz !");
+                Console.WriteLine(e);
+
+            }finally{
+                Console.WriteLine("İşlem tamamlandı !");
+            }
+
+        }
+    }
+}
